20b3b051a5dad052a26c9d138e007f5e8143775e
HGF50 <haccandypaulo@gmail.com>
2026-01-04 22:34:00 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("dropweh-v1").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js"
-      ]);
-    })
-  );
-});
+
